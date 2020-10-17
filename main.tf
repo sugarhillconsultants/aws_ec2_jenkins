f8b2ab8ad0a556ec92660354933b064981f1f4f6
pyenugula <pkyenugula@gmail.com>
2020-10-17 13:07:11 +0530

    updated aws_ec2_instance.git

--- main.tf
+++ main.tf
@@ -56,8 +56,12 @@ resource "aws_key_pair" "keypair" {
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCzHd9+N+t2I3lGrcD7jvgiymPidJCGdgC39dwlLDARR6dYsmWGWkou47sMKmnSUxNLebw8ThCW0aeP1CDANnd8H8BkCyEq1OKKEolNm+rfm026qwNWRyH4l2h16YkPA+NAfG9jLBRMWxXTZIjhCJoFE1EDUjIfriSimvpeVfaGBqQ6i5p5xeXGDyTqpuCzrlo4wWkGBKmgh79p0chJlIVx/Z5r8vZMlGQz4PnznErpA4X2iwbB6WCOL5XQBK9FhKHjPM49SDUVHajk7tHQlz04YQDrpESgydq/C7sHjzb1vi309gh1023zj1f6XdlL64ENCUcDbqynJPXTuxFDlAFUqDmPfzqLJN5ozYocSCl7FRxuHjwsdMDH/xeu0DoUhlDDISGqDzoM8Hci1HGP1UCqHTgU9VQbuLbI+tgTQzM4VU83u7BY5aWZBMT+mEjPrIY2qdFCQRWCAmo7+rqygA48qdhY6Zoec9LxKsY6Oonp0yg5A3y5o2A/TpeqxsR7tTs= pavankumar@Pavans-Air.lan"
 }
 
-resource "aws_s3_bucket" "spectrait.tk" {
+#resource "aws_s3_bucket" "spectrait.tk" {
 
-}
+
+
+
+
+
 
 
